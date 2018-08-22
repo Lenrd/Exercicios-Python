f62329beb21fbac4911c2f2cb8b86623deef4e85
lenrd <leonardo_aguiar@live.co.kr>
2018-08-21 21:19:01 -0300

    89

--- Desafio089.py
+++ Desafio089.py
@@ -8,7 +8,7 @@ while True:
     nome = str(input('Digite o Nome: '))
     n1 = float(input('Digite a primeira nota: '))
     n2 = float(input('Digite a segunda nota: '))
-    n3 = n1 + n2 / 2
+    n3 = (n1 + n2) / 2
 
     aluno.append(nome)
     notas.append(n1)
@@ -24,5 +24,13 @@ while True:
     if conf == 'N':
         break
 
-for a in alunos:
-    print(a)
+print('-=-' * 10)
+print(f'{"NUM":<5}{"NOME":<12}{"MÉDIA"}')
+for a in range(len(alunos)):
+    print(f'{str(a):<5}{alunos[a][0]:<12}{str(alunos[a][1][2])}')
+
+
+while True:
+    num = int(input('mostrar notas de qual aluno: '))
+
+    print(f'{alunos[num][0]:<12}Nota1: {str(alunos[num][1][0]):<5}, nota2: {str(alunos[num][1][1])}')
